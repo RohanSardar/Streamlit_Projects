c3cfc66e71722bc4b59536ec62ebe1c3dd17d7d3
Rohan Sardar <77870108+RohanSardar@users.noreply.github.com>
2024-02-09 22:19:24 +0530

    Create chart_plotter.py

---
+++ chart_plotter.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("Chart Plotter")
+
+ops = st.selectbox(
+    "Type of chart",
+    ('Line Chart', 'Scatter Chart')
+)
+
+col1, col2 = st.columns(2)
+x = col1.number_input('Lower range', value=None, placeholder='Type lower range...')
+y = col2.number_input('Upper range', value=None, placeholder='Type upper range...')
+
+n = st.slider('Number of random variables', 0, 100)
+
+chart_data = pd.DataFrame(np.random.randint(int(x), int(y)+1, int(n)), columns=["a"])
+
+st.write('### Output')
+
+if ops == 'Line Chart':
+    st.line_chart(chart_data)
+elif ops == 'Scatter Chart':
+    st.scatter_chart(chart_data)
