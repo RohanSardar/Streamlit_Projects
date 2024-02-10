bb92bc43dda28403e9602e9d11fe5172575e5717
Rohan Sardar <77870108+RohanSardar@users.noreply.github.com>
2024-02-11 01:19:09 +0530

    Update chart_plotter.py

--- chart_plotter.py
+++ chart_plotter.py
@@ -3,6 +3,37 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+def random_plot(ops):
+    col1, col2 = st.columns(2)
+    x = col1.number_input('Lower range', value=None, placeholder='Type lower range...')
+    y = col2.number_input('Upper range', value=None, placeholder='Type upper range...')
+    n = st.slider('Number of random variables', 0, 100)
+    if x and y and n:
+        st.write('### Output')
+        chart_data = pd.DataFrame(np.random.randint(int(x), int(y) + 1, int(n)), columns=["1"])
+        if ops == 'Line Chart':
+            st.line_chart(chart_data)
+        else:
+            st.scatter_chart(chart_data)
+
+def custom_plot(ops):
+    n = st.text_input('Data points',
+                      placeholder="Type as many data points as you can")
+    if len(n) > 0:
+        if ' ' in n:
+            n = np.array(n.split()).astype('int')
+        else:
+            if len(n) > 0 and n[-1] == ',':
+                n = n[:-1]
+                n = np.array(n.split(',')).astype('int')
+
+        st.write('### Output')
+
+        if ops == 'Line Chart':
+            st.line_chart(n)
+        else:
+            st.scatter_chart(n)
+
 st.title("Chart Plotter")
 
 ops = st.selectbox(
@@ -10,18 +41,12 @@ ops = st.selectbox(
     ('Line Chart', 'Scatter Chart')
 )
 
-col1, col2 = st.columns(2)
-x = col1.number_input('Lower range', value=None, placeholder='Type lower range...')
-y = col2.number_input('Upper range', value=None, placeholder='Type upper range...')
+custom = st.toggle('Custom data mode')
 
-n = st.slider('Number of random variables', 0, 100)
+if custom == True:
+    st.write('###### Currently in custom mode')
+    custom_plot(ops)
 
-if x and y:
-    st.write('### Output')
-
-    chart_data = pd.DataFrame(np.random.randint(int(x), int(y)+1, int(n)), columns=["a"])
-    
-    if ops == 'Line Chart':
-        st.line_chart(chart_data)
-    elif ops == 'Scatter Chart':
-        st.scatter_chart(chart_data)
+else:
+    st.write('###### Currently in random mode')
+    random_plot(ops)
